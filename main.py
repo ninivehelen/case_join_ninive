0d03bd52b2532bfe9b01a9ddf06389105d5b5f69
Nínive Helen <ninivehelen@gmail.com>
2023-08-25 14:26:55 -0300

    Criação do DER, script para extrair os dados da tabela e salvar em formato csv e paquet

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ import pyarrow.parquet as pq
 lista_add_colunas = []
 colunas = ['customers','employees','offices','orderdetails','orders','payments','product_lines','products']
 
-cabecalho1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
-cabecalho2 = ['1','2','3','4','5','6','7','8']
-
-
 def conexao():
     con = psycopg2.connect(host='psql-mock-database-cloud.postgres.database.azure.com', database='ecom1692848222792bypjwxeymatmczia',
     user='gkwimjzggfdatutzxsdmvjzq@psql-mock-database-cloud', password='ntikuznbgqhgqqbsydxnycdu')
